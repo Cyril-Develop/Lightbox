94a2834661e05af18e11a7e49bb92fbc550e55c7
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2023-08-23 13:58:07 +0200

    init

---
+++ datas.js
@@ -0,0 +1,44 @@
+export const datas = [
+    {
+        title: "Forest in the light",
+        image: "Forest_in_the_light.jpg",
+        likes: 34,
+        date: "2021-06-01",
+        id: 1
+    },
+    {
+        title: "Road to adventure",
+        image: "Road_to_adventure.jpg",
+        likes: 45,
+        date: "2019-06-02",
+        id: 2
+    },
+    {
+        title: "Lake in the mountains",
+        image: "Lake_in_the_mountains.jpg",
+        likes: 67,
+        date: "2020-06-03",
+        id: 3
+    },
+    {
+        title: "Moon in the night",
+        image: "Moon_in_the_night.jpg",
+        likes: 90,
+        date: "1988-06-04",
+        id: 4
+    },
+    {
+        title: "Rainbow in nature",
+        image: "Rainbow_in_nature.jpg",
+        likes: 34,
+        date: "2021-09-05",
+        id: 5
+    },
+    {
+        title: "Skycrapers at night",
+        image: "Skycrapers_at_night.jpg",
+        likes: 79,
+        date: "2008-06-06",
+        id: 6
+    }
+];
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Lightbox in Javascript
+
+![screenshot](./images/screenshot/screenshot.png)
+
+![GitHub repo size](https://img.shields.io/github/repo-size/Cyril-Develop/Lightbox?style=for-the-badge) 
+
+ ## Tester le projet :
+
+```terminal
+git clone https://github.com/Cyril-Develop/Lightbox.git
+```
